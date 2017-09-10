a586d060c17572f33ae841c8c745c1107f2ae4bd
林业 <31615967@qq.com>
2017-09-10 23:35:01 +0800

    添加返回自定义状态码

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,9 +7,13 @@ import org.omg.CORBA.UserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -28,29 +32,53 @@ public class UserController {
             @ApiImplicitParam(name = "user", required = true, value = "用户的密码", dataType = "User")
     })
     @ApiResponses({
-            @ApiResponse(code = 400, message = "请求参数没填好"),
-            @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
+            @ApiResponse(code = 400, message = "请求参数没填好")
     })
     @PostMapping(value = "/")
-    public User addUser(@RequestBody User user) throws UserException {
-        return userService.addUser(user);
+    public ResponseEntity<Map<String, Object>> addUser(@RequestBody User user) throws UserException {
+        Map<String, Object> map = new HashMap<>();
+        User res = userService.addUser(user);
+        if (res == null) {
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+
+        map.put("data", res);
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
     @ApiOperation("根据ID查询用户信息")
     @ApiImplicitParam(paramType = "path", name = "id", dataType = "Long", required = true, value = "用户ID")
     @GetMapping(value = "/{id}")
-    public User findById(@PathVariable("id") Integer id) {
-        logger.debug("id:" + id);
-        return userService.fineById(id);
+    public ResponseEntity<Map<String, Object>> findById(@PathVariable("id") Integer id) {
+        Map<String, Object> map = new HashMap<>();
+        User user = userService.fineById(id);
+        if (user == null) {
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+
+        map.put("data", user);
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
     @ApiOperation(value = "获取用户列表")
     @ApiImplicitParams({
-            @ApiImplicitParam(paramType = "path", name = "pageNum", dataType = "Long", value = "页码", defaultValue = "1"),
-            @ApiImplicitParam(paramType = "path", name = "pageSize", dataType = "Long", value = "每页大小", defaultValue = "10")
+            @ApiImplicitParam(paramType = "query", name = "pageNum", dataType = "Long", value = "页码", defaultValue = "1"),
+            @ApiImplicitParam(paramType = "query", name = "pageSize", dataType = "Long", value = "每页大小", defaultValue = "10")
     })
-    @GetMapping("/list/{pageNum}/{pageSize}")
-    public List<User> list(@PathVariable("pageNum") Integer pageNum, @PathVariable("pageSize") Integer pageSize) {
-        return userService.list(pageNum, pageSize);
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "请求参数没填好"),
+            @ApiResponse(code = 404, message = "没有找到")
+    })
+    @GetMapping("/")
+    public ResponseEntity<Map<String, Object>> list(
+            @RequestParam("pageNum") Integer pageNum,
+            @RequestParam("pageSize") Integer pageSize)
+            throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        map.put("data", userService.list(pageNum, pageSize));
+        if (map.get("data") == null) {
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/demo/handle/ExceptionHandle.java
+++ src/main/java/com/example/demo/handle/ExceptionHandle.java
@@ -5,6 +5,7 @@ import com.example.demo.exception.SiteException;
 import com.example.demo.utils.ResultUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,9 +20,13 @@ public class ExceptionHandle {
         if (e instanceof SiteException) {
             SiteException siteException = (SiteException) e;
             return ResultUtil.error(siteException.getCode(), siteException.getMessage());
-        } else {
-            logger.error("[系统异常] {}", e);
-            return ResultUtil.error(-1, "未知错误");
+        }
+        else if(e instanceof MissingServletRequestParameterException){
+            return ResultUtil.error(400, "参数错误");
+        }
+        else {
+            logger.error("[Catch a exception] {}", e);
+            return ResultUtil.error(-1, "Unknown error");
         }
     }
 }
